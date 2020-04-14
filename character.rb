4c4de389740f452ec49c1cc8215ef092344f3678
William Acevedo <william.acevedo@gmail.com>
2020-04-15 00:41:46 +0200

    Began password test cases

--- character.rb
+++ character.rb
@@ -3,5 +3,20 @@ class Character
   
   def initialize(name)
     @name = name
+    @password = ''
+  end
+  
+  def change_password(old_password, new_password)
+    if @password.empty?
+      @password = new_password
+      'Password assigned'
+    elsif
+      if old_password == @password
+        @password = new_password
+        'Password changed'
+      else
+        'not changed'
+      end
+    end
   end
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -1,13 +1,22 @@
 require_relative '../character.rb'
-
 RSpec.describe 'A D&D character' do
+  let(:player) { Character.new('William') }
+  
   it 'Can be assinged a name' do
-    name = 'William'
-    player01 = Character.new(name)
-    
-    player01name = player01.name
-
-    expect(player01name).not_to be_empty
-    expect(player01name).to eq('William')
+    expect(player.name).not_to be_empty
+    expect(player.name).to eq('William')
   end
+
+  it 'can be assigned a password' do
+    password = '123456'
+
+    confirmed = player.change_password('', password)
+    expect(confirmed).to eq('Password assigned')
+  end
+
+  it 'can check that a password is set' do
+    pending 'test not implemented yet.'  
+    expect(player.password?).to eq('Password not assigned')
+  end
+  it 'can confirm and change a password'
 end
